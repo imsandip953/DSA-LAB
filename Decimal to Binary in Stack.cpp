659e9af81444961495685cf1f2b7f932b291d13b
Sandip <thisizsandip@gmail.com>
2025-05-06 12:56:13 +0600

    Create Decimal to Binary in Stack.cpp

---
+++ Decimal to Binary in Stack.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 10
+int stack1[MAX], top = -1;
+void Push(int val){
+    if(top == MAX - 1){
+        cout << "Stack Overflow" << endl;
+        return;
+    }
+    top++;
+    stack1[top] = val;
+}
+int Pop(){
+    if(top == -1){
+        cout << "Stack Underflow!!!" << endl;
+    }
+    top--;
+    return stack1[top + 1];
+}
+void Print(){
+    if(top == -1){
+        cout << "Empty Stack" << endl;
+    }
+    for(int i = 0; i <= top; i++){
+        cout << stack1[i] << " ";
+    }
+    cout << endl;
+}
+int main(){
+    while(1){
+        int dec, val, del;
+        cin >> dec;
+        while(dec > 0){
+            val = dec % 2;
+            dec = dec / 2;
+            Push(val);
+
+        }
+
+        while(1){
+           del = Pop();
+           cout << del << " ";
+           if(top == -1){
+            break;
+           }
+        }
+
+    }
+
+}
